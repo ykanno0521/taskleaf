a5f18bbecff4159e58a96f6b8d6edcb5570659f2
ykanno0521 <kanno0521@gmail.com>
2020-01-02 10:52:40 +0900

    Add:nameのvalidationを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_01_053440) do
+ActiveRecord::Schema.define(version: 2020_01_02_005933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
