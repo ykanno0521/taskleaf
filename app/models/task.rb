53b65b860daa70a8b3549893b1729f6a91a218f5
ykanno0521 <kanno0521@gmail.com>
2020-01-02 12:39:44 +0900

    Add:bcryptを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
+  # validates :name, presence: true
+  # validates :name, lengthi: { maximum: 30}
+  validates :name, presence: true, length: { maximum: 30}
+  validate :validate_name_not_including_comma
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
