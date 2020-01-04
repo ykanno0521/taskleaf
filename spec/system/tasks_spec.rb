25c3cfba2bc3f4deaaf1cc97b23802967e8e7bfd
ykanno0521 <kanno0521@gmail.com>
2020-01-04 14:37:57 +0900

    Add:詳細表示機能rspec追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,43 +1,48 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
+
+  before do
+    visit login_path #ログイン画面にアクセスする
+    fill_in 'メールアドレス', with: login_user.email #メールアドレスを入力する
+    fill_in 'パスワード', with: login_user.password #パスワードを入力する
+    click_button 'ログインする' #「ログインする」ボタンを押す
+  end
+
   describe '一覧表示機能' do
-    before do
-      # ユーザーAを作成しておく
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
-      # 作成者がユーザーAであるタスクを作成しておく
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a )
-    end
     context 'ユーザーAがログインしているとき' do
-      before do
-        # ユーザーAでログインする
-        visit login_path #ログイン画面にアクセスする
-        fill_in 'メールアドレス', with: 'a@example.com' #メールアドレスを入力する
-        fill_in 'パスワード', with: 'password' #パスワードを入力する
-        click_button 'ログインする' #「ログインする」ボタンを押す
-      end
+      let(:login_user) { user_a }
 
       it 'ユーザーAが作成したタスクが表示される' do
         # 作成済みのタスクの名称が画面上に表示されることを確認
         expect(page).to have_content '最初のタスク'
       end
+    end
     
     context 'ユーザーBがログインしているとき' do
-      before do
-        # ユーザーBを作成しておく
-        FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
-        # ユーザーBでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_b }
 
       it 'ユーザーAが作成したタスクが表示されない' do
         # ユーザーAが作成したタスクの名称が画面上に表示されないことを確認
         expect(page).to have_no_content '最初のタスク'
       end
     end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
+      end
     end
   end
 end
