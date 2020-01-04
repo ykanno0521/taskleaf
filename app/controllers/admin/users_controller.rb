a107359338dcf8090b8c3720a5c80d41e7c29705
ykanno0521 <kanno0521@gmail.com>
2020-01-04 10:37:51 +0900

    Add:admin機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+  before_action :require_admin
   def index
     @users = User.all
   end
@@ -47,5 +48,9 @@ class Admin::UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def require_admin
+    redirect_to root_url, unless current_user.admin?
   end
 end
