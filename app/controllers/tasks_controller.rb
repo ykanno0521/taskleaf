4a32af03d93b18a492561bb9fa2c4de6c3c4aff9
ykanno0521 <kanno0521@gmail.com>
2020-01-04 10:54:24 +0900

    Add:set_taskメソッドの定義、共通化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
     #@tasks = Task.where(user_id: current_user_id)と同様
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
+    # @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -29,23 +31,29 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
+    # @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    # task = current_user.tasks.find(params[:id])
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}を削除しました」"
+    # task = current_user.tasks.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}を削除しました」"
   end
 
   private
   def task_params
     params.require(:task).permit(:name, :description)
   end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+
+
 end
