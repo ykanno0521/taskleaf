4ec263618dfc9ed00d4b1b4c96748b6c6577051f
ykanno0521 <kanno0521@gmail.com>
2020-01-05 10:14:06 +0900

    Fix:日本時間に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :login_required
+  # before_action :set_locale
 
   private
 
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def login_required
     redirect_to login_url unless current_user
+  end
+
+  def set_locale
+    # I18n.locale = current_user&.locale || :ja #ログインしていなければ日本語
   end
 end
