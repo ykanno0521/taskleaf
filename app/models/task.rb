7be6c2bfc299cf6ba02458b552f78e5517f3ed6a
ykanno0521 <kanno0521@gmail.com>
2020-01-05 13:51:47 +0900

    Fix:検索条件を絞るよう設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,6 +8,14 @@ class Task < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc) }
 
+  # 検索条件を絞る
+  def self.ransackable_attrbutes(auth_object = nil) # 検索対象のカラムを指定
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil) # 空の配列を返すことで、意図しない関連を含めないようにする
+    []
+  end
   private
 
   def validate_name_not_including_comma
