63f23f1faa7afaf6594ad8e88a659ed221450daa
ykanno0521 <kanno0521@gmail.com>
2020-01-06 15:50:55 +0900

    Add:タスク背景色変更

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,10 @@
+document.addEventListener('turbolinks:load', function() {
+  document.querySelectorAll('td').forEach(function(td) {
+    td.addEventListener('mouseover', function(e) {
+      e.currentTarget.style.backgroundColor = '#eff';
+    });
+    td.addEventListener('mouseout', function(e) {
+      e.currentTarget.style.backgroundColor = '';
+    });
+  });
+});
